d91d4057f65fa47d3c1930a2bc026df85664e647
Wouter de Vos <wouter@codaisseur.com>
2016-09-05 12:25:01 +0200

    Add auth to games endpoint

--- src/services/game/hooks/index.js
+++ src/services/game/hooks/index.js
@@ -1,11 +1,15 @@
 'use strict';
 
 const globalHooks = require('../../../hooks');
-const hooks = require('feathers-hooks');
+const auth = require('feathers-authentication').hooks;
 
 
 exports.before = {
-  all: [],
+  all: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToAuthenticated()
+  ],
   find: [],
   get: [],
   create: [],
