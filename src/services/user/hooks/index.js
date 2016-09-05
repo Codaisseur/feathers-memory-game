ac74dd28faff1a955c1c3460c74c832b1c164ba2
Wouter de Vos <wouter@codaisseur.com>
2016-09-05 12:29:18 +0200

    Add hook to add Gravatars for new users

--- src/services/user/hooks/index.js
+++ src/services/user/hooks/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const gravatar = require('./gravatar');
+
 const globalHooks = require('../../../hooks');
 const hooks = require('feathers-hooks');
 const auth = require('feathers-authentication').hooks;
@@ -17,9 +19,7 @@ exports.before = {
     auth.restrictToAuthenticated(),
     auth.restrictToOwner({ ownerField: '_id' })
   ],
-  create: [
-    auth.hashPassword()
-  ],
+  create: [auth.hashPassword(), gravatar()],
   update: [
     auth.verifyToken(),
     auth.populateUser(),
