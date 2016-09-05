3716496cf3e81e79ffb7fd056b2e7490acab2ad7
Wouter de Vos <wouter@codaisseur.com>
2016-09-05 12:00:05 +0200

    Game schema and endpoint

--- src/services/user/user-model.js
+++ src/services/user/user-model.js
@@ -9,9 +9,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  email: {type: String, required: true, unique: true},
+  name: { type: String, required: false },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  
+
   createdAt: { type: Date, 'default': Date.now },
   updatedAt: { type: Date, 'default': Date.now }
 });
